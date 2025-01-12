a5800054a40b6f6e08432e4eeaadf8a8d77347b7
Rohan1-max <rohan.krishnan16@gmail.com>
2025-01-12 22:40:57 +0530

    updated link

--- dbConfig/config.js
+++ dbConfig/config.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/carRegistry")
-//   .connect("mongodb+srv://Rohan:1234567890@cluster0.matpx.mongodb.net/retail-dashboard?retryWrites=true&w=majority&appName=Cluster0")
+  // .connect("mongodb://localhost:27017/carRegistry")
+  .connect("mongodb+srv://Rohan:1234567890@cluster0.matpx.mongodb.net/retail-dashboard?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
   
